5941fef9e72c742b0f6ea3b9a1d181fc9f57cd8c
Samuel Simpson <samjsimpson95@gmail.com>
2016-01-04 16:32:10 -0500

    Changing build process to use makefile. Cleaning up project files.

--- Image_Edge_Detection/Debug/src/lodepng.d
+++
@@ -1,4 +0,0 @@
-src/lodepng.d: ../src/lodepng.cpp \
- /home/sam/Coding_Projects/Edge_Detection/Image_Edge_Detection/include/lodepng.h
-
-/home/sam/Coding_Projects/Edge_Detection/Image_Edge_Detection/include/lodepng.h:
